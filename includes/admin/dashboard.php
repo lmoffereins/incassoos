16c56b937b1ea49327266562e6c1341a58c40552
Laurens Offereins <lmoffereins@gmail.com>
2020-08-25 21:35:28 +0200

    Dashboard: introduce 'Uncollected items' dashboard widget and modify 'History' widget into 'Recent activity'

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -65,11 +65,18 @@ function incassoos_admin_add_dashboard_widgets() {
 		'incassoos_admin_dashboard_status_widget'
 	);
 
-	// History
+	// Recent activity
 	wp_add_dashboard_widget(
-		'incassoos_dashboard_history',
-		_x( 'History', 'Dashboard widget', 'incassoos' ),
-		'incassoos_admin_dashboard_history_widget'
+		'incassoos_dashboard_recent',
+		_x( 'Recent activity', 'Dashboard widget', 'incassoos' ),
+		'incassoos_admin_dashboard_recent_widget'
+	);
+
+	// Uncollected items
+	wp_add_dashboard_widget(
+		'incassoos_dashboard_uncollected',
+		_x( 'Uncollected items', 'Dashboard widget', 'incassoos' ),
+		'incassoos_admin_dashboard_uncollected_widget'
 	);
 
 	// Quick actions
@@ -101,13 +108,16 @@ function incassoos_admin_dashboard_status_widget() {
 	$order_count      = wp_count_posts( $order      );
 	$product_count    = wp_count_posts( $product    );
 
+	// Pre-calculate
+	$collections = $collection_count->publish + $collection_count->{incassoos_get_staged_status_id()};
+
 	// Collect statuses to display
 	$statuses = apply_filters( 'incassoos_admin_dashboard_statuses', array(
 
 		// Collections
 		'collection-count' => sprintf( '<a href="%s">%s</a>',
 			esc_url( add_query_arg( array( 'post_type' => $collection, 'post_status' => 'publish' ), admin_url( 'edit.php' ) ) ),
-			sprintf( _n( '%s Collection', '%s Collections', $collection_count->publish, 'incassoos' ), $collection_count->publish )
+			sprintf( _n( '%s Collection', '%s Collections', $collections, 'incassoos' ), $collections )
 		),
 
 		// Activities
@@ -159,76 +169,205 @@ function incassoos_admin_dashboard_status_widget() {
 }
 
 /**
- * Output the contents of the History dashboard widget
+ * Output the contents of the Recent activity dashboard widget
  *
  * @since 1.0.0
  */
-function incassoos_admin_dashboard_history_widget() {
+function incassoos_admin_dashboard_recent_widget() {
 
-	// Assets
-	$collection = incassoos_get_collection_post_type();
-	$activity   = incassoos_get_activity_post_type();
-	$occasion   = incassoos_get_occasion_post_type();
-	$order      = incassoos_get_order_post_type();
+	echo '<div class="main">';
 
-	// Counts
-	$collection_count = wp_count_posts( $collection );
-	$activity_count   = wp_count_posts( $activity   );
-	$occasion_count   = wp_count_posts( $occasion   );
-	$order_count      = wp_count_posts( $order      );
-
-	// Status
-	$collected = incassoos_get_collected_status_id();
-
-	// Collect statuses to display
-	$statuses = apply_filters( 'incassoos_admin_dashboard_statuses', array(
-
-		// Collections
-		'collection-count' => sprintf( '<a href="%s">%s</a>',
-			esc_url( add_query_arg( array( 'post_type' => $collection, 'post_status' => $collected ), admin_url( 'edit.php' ) ) ),
-			sprintf( _n( '%s Collection', '%s Collections', $collection_count->{$collected}, 'incassoos' ), $collection_count->{$collected} )
-		),
-
-		// Activities
-		'activity-count' => sprintf( '<a href="%s">%s</a>',
-			esc_url( add_query_arg( array( 'post_type' => $activity, 'post_status' => $collected ), admin_url( 'edit.php' ) ) ),
-			sprintf( _n( '%s Activity', '%s Activities', $activity_count->{$collected}, 'incassoos' ), $activity_count->{$collected} )
-		),
-
-		// Occasions
-		'occasion-count' => sprintf( '<a href="%s">%s</a>',
-			esc_url( add_query_arg( array( 'post_type' => $occasion, 'post_status' => $collected ), admin_url( 'edit.php' ) ) ),
-			sprintf( _n( '%s Occasion', '%s Occasions', $occasion_count->{$collected}, 'incassoos' ), $occasion_count->{$collected} )
-		),
-
-		// Orders
-		'order-count' => sprintf( '<a href="%s">%s</a>',
-			esc_url( add_query_arg( array( 'post_type' => $order, 'post_status' => $collected ), admin_url( 'edit.php' ) ) ),
-			sprintf( _n( '%s Order', '%s Orders', $order_count->{$collected}, 'incassoos' ), $order_count->{$collected} )
-		),
+	$recent_collections = incassoos_admin_dashboard_recent_posts( array(
+		'id'         => 'recently-collected',
+		'title'      => esc_html__( 'Recently collected', 'incassoos' ),
+		'total_cb'   => 'incassoos_get_collection_total',
+		'max'        => 5,
+		'status'     => incassoos_get_collected_status_id(),
+		'post_type'  => incassoos_get_collection_post_type(),
+		'order'      => 'DESC',
+		'orderby'    => 'meta_collected',
+		'meta_query' => array(
+			'relation'       => 'AND',
+			'meta_collected' => array(
+				'key'     => 'collected',
+				'compare' => 'EXISTS'
+			)
+		)
 	) );
 
-	?>
+	$recent_orders = incassoos_admin_dashboard_recent_posts( array(
+		'id'        => 'recently-ordered',
+		'title'     => esc_html__( 'Recently ordered', 'incassoos' ),
+		'total_cb'   => 'incassoos_get_order_total',
+		'max'       => 10,
+		'status'    => 'any',
+		'post_type' => incassoos_get_order_post_type(),
+		'order'     => 'DESC'
+	) );
 
-	<div class="main">
-		<?php if ( ! empty( $statuses ) ) : ?>
+	if ( ! $recent_collections && ! $recent_orders ) {
+		echo '<div class="no-activity">';
+		echo '<p>' . esc_html__( 'No activity yet!' ) . '</p>';
+		echo '</div>';
+	}
 
-		<ul>
-			<?php foreach ( $statuses as $status => $label ) : ?>
+	echo '</div>';
+}
 
-			<li class="<?php echo esc_attr( $status ); ?>"><?php echo $label; ?></li>
+/**
+ * Output the contents of the Uncollected items dashboard widget
+ *
+ * @since 1.0.0
+ */
+function incassoos_admin_dashboard_uncollected_widget() {
 
-			<?php endforeach; ?>
-		</ul>
+	echo '<div class="main">';
 
-		<?php else : ?>
+	$recent_collections = incassoos_admin_dashboard_recent_posts( array(
+		'id'             => 'recently-collected',
+		'title'          => esc_html__( '%d Collections', 'incassoos' ),
+		'total_cb'       => 'incassoos_get_collection_total',
+		'count_in_title' => true,
+		'max'            => -1,
+		'status'         => array( 'publish', incassoos_get_staged_status_id() ),
+		'post_type'      => incassoos_get_collection_post_type(),
+		'order'          => 'DESC',
+	) );
 
-		<p class="description"><?php esc_html_e( 'There is currently nothing to display here.', 'incassoos' ); ?></p>
+	$recent_activities = incassoos_admin_dashboard_recent_posts( array(
+		'id'             => 'uncollected-activities',
+		'title'          => esc_html__( '%d Activities', 'incassoos' ),
+		'total_cb'       => 'incassoos_get_activity_total',
+		'count_in_title' => true,
+		'max'            => -1,
+		'status'         => 'publish',
+		'post_type'      => incassoos_get_activity_post_type(),
+		'order'          => 'DESC',
+	) );
 
-		<?php endif; ?>
-	</div>
+	$recent_occasions = incassoos_admin_dashboard_recent_posts( array(
+		'id'             => 'uncollected-occasions',
+		'title'          => esc_html__( '%d Occasions', 'incassoos' ),
+		'total_cb'       => 'incassoos_get_occasion_total',
+		'count_in_title' => true,
+		'max'            => -1,
+		'status'         => 'publish',
+		'post_type'      => incassoos_get_occasion_post_type(),
+		'order'          => 'DESC',
+	) );
 
-	<?php
+	if ( ! $recent_collections && ! $recent_activities && ! $recent_occasions && ! $recent_orders ) {
+		echo '<div class="no-activity">';
+		echo '<p>' . esc_html__( 'No collectable items yet!', 'incassoos' ) . '</p>';
+		echo '</div>';
+	}
+
+	echo '</div>';
+}
+
+/**
+ * Generates Publishing Soon and Recently Published sections.
+ *
+ * @see wp_dashboard_recent_posts()
+ *
+ * @since 1.0.0
+ *
+ * @param array $args {
+ *     An array of query and display arguments.
+ *
+ *     @type int    $max     Number of posts to display.
+ *     @type string $status  Post status.
+ *     @type string $order   Designates ascending ('ASC') or descending ('DESC') order.
+ *     @type string $title   Section title.
+ *     @type string $id      The container id.
+ * }
+ * @return bool False if no posts were found. True otherwise.
+ */
+function incassoos_admin_dashboard_recent_posts( $args ) {
+	$query_args = wp_parse_args( $args, array(
+		'post_type'      => 'post',
+		'post_status'    => $args['status'],
+		'orderby'        => 'date',
+		'order'          => $args['order'],
+		'posts_per_page' => intval( $args['max'] ),
+		'no_found_rows'  => true,
+		'cache_results'  => false,
+		'perm'           => ( 'future' === $args['status'] ) ? 'editable' : 'readable',
+	) );
+
+	// Remove non-query args
+	unset( $query_args['id'], $query_args['title'] );
+
+	/**
+	 * Filters the query arguments used for the Recent Posts widget.
+	 *
+	 * @since 4.2.0
+	 *
+	 * @param array $query_args The arguments passed to WP_Query to produce the list of posts.
+	 */
+	$query_args = apply_filters( 'dashboard_recent_posts_query_args', $query_args );
+	$posts      = new WP_Query( $query_args );
+
+	if ( $posts->have_posts() ) {
+
+		echo '<div id="' . $args['id'] . '" class="activity-block recent-posts">';
+
+		if ( isset( $args['count_in_title'] ) && ! empty( $args['count_in_title'] ) ) {
+			$args['title'] = sprintf( $args['title'], $posts->post_count );
+		}
+
+		echo '<h3>' . $args['title'] . '</h3>';
+
+		echo '<ul>';
+
+		$today    = current_time( 'Y-m-d' );
+		$tomorrow = current_datetime()->modify( '+1 day' )->format( 'Y-m-d' );
+		$year     = current_time( 'Y' );
+
+		while ( $posts->have_posts() ) {
+			$posts->the_post();
+
+			$time = get_the_time( 'U' );
+			if ( gmdate( 'Y-m-d', $time ) == $today ) {
+				$relative = __( 'Today' );
+			} elseif ( gmdate( 'Y-m-d', $time ) == $tomorrow ) {
+				$relative = __( 'Tomorrow' );
+			} elseif ( gmdate( 'Y', $time ) !== $year ) {
+				/* translators: Date and time format for recent posts on the dashboard, from a different calendar year, see https://www.php.net/date */
+				$relative = date_i18n( __( 'M jS Y' ), $time );
+			} else {
+				/* translators: Date and time format for recent posts on the dashboard, see https://www.php.net/date */
+				$relative = date_i18n( __( 'M jS' ), $time );
+			}
+
+			// Use the post edit link for those who can edit, the permalink otherwise.
+			$recent_post_link = current_user_can( 'edit_post', get_the_ID() ) ? get_edit_post_link() : get_permalink();
+
+			// Get the post's total
+			$post_total = isset( $args['total_cb'] ) && is_callable( $args['total_cb'] ) ? call_user_func_array( $args['total_cb'], array( 0, true ) ) : false;
+
+			$draft_or_post_title = _draft_or_post_title();
+			printf( '<li><span class="post-date">%1$s</span> <a href="%2$s" aria-label="%3$s">%4$s</a> %5$s</li>',
+				/* translators: 1: Relative date, 2: Time. */
+				sprintf( _x( '%1$s, %2$s', 'dashboard' ), $relative, get_the_time() ),
+				$recent_post_link,
+				/* translators: %s: Post title. */
+				esc_attr( sprintf( __( 'Edit &#8220;%s&#8221;' ), $draft_or_post_title ) ),
+				$draft_or_post_title,
+				! empty( $post_total ) ? '<span class="post-total">' . $post_total . '</span>' : ''
+			);
+		}
+
+		echo '</ul>';
+		echo '</div>';
+
+	} else {
+		return false;
+	}
+
+	wp_reset_postdata();
+
+	return true;
 }
 
 /**
