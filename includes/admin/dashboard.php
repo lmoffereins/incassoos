c5aa9bd56cbf4df35f47c0abf8dd60c5305a62af
Laurens Offereins <lmoffereins@gmail.com>
2020-08-25 12:47:00 +0200

    Dashboard: add the Quick Actions dashboard widget

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -62,14 +62,21 @@ function incassoos_admin_add_dashboard_widgets() {
 	wp_add_dashboard_widget(
 		'incassoos_dashboard_status',
 		__( 'At a Glance' ),
-		'incassoos_admin_dashboard_status'
+		'incassoos_admin_dashboard_status_widget'
 	);
 
 	// History
 	wp_add_dashboard_widget(
 		'incassoos_dashboard_history',
 		_x( 'History', 'Dashboard widget', 'incassoos' ),
-		'incassoos_admin_dashboard_history'
+		'incassoos_admin_dashboard_history_widget'
+	);
+
+	// Quick actions
+	wp_add_dashboard_widget(
+		'incassoos_dashboard_actions',
+		_x( 'Quick actions', 'Dashboard widget', 'incassoos' ),
+		'incassoos_admin_dashboard_actions_widget'
 	);
 }
 
@@ -78,7 +85,7 @@ function incassoos_admin_add_dashboard_widgets() {
  *
  * @since 1.0.0
  */
-function incassoos_admin_dashboard_status() {
+function incassoos_admin_dashboard_status_widget() {
 
 	// Assets
 	$collection = incassoos_get_collection_post_type();
@@ -156,7 +163,7 @@ function incassoos_admin_dashboard_status() {
  *
  * @since 1.0.0
  */
-function incassoos_admin_dashboard_history() {
+function incassoos_admin_dashboard_history_widget() {
 
 	// Assets
 	$collection = incassoos_get_collection_post_type();
@@ -213,6 +220,57 @@ function incassoos_admin_dashboard_history() {
 
 			<?php endforeach; ?>
 		</ul>
+
+		<?php else : ?>
+
+		<p class="description"><?php esc_html_e( 'There is currently nothing to display here.', 'incassoos' ); ?></p>
+
+		<?php endif; ?>
+	</div>
+
+	<?php
+}
+
+/**
+ * Output the contents of the Quick actions dashboard widget
+ *
+ * @since 1.0.0
+ */
+function incassoos_admin_dashboard_actions_widget() {
+	$actions = array();
+
+	// Assets
+	$assets = array(
+		incassoos_get_collection_post_type(),
+		incassoos_get_activity_post_type(),
+		incassoos_get_occasion_post_type(),
+		incassoos_get_order_post_type(),
+		incassoos_get_product_post_type()
+	);
+
+	// Define actions
+	foreach ( $assets as $post_type ) {
+		$post_type_object = get_post_type_object( $post_type );
+
+		$actions[ "new-{$post_type}" ] = sprintf( '<a class="button button-secondary" href="%s">%s</a>',
+			esc_url( add_query_arg( array( 'post_type' => $post_type ), admin_url( 'post-new.php' ) ) ),
+			$post_type_object->labels->add_new
+		);
+	}
+
+	// Collect actions to display
+	$actions = apply_filters( 'incassoos_admin_dashboard_actions', $actions );
+
+	?>
+
+	<div class="main">
+		<?php if ( ! empty( $actions ) ) : ?>
+
+			<?php foreach ( $actions as $status => $label ) : ?>
+
+			<span class="<?php echo esc_attr( $status ); ?>"><?php echo $label; ?></span>
+
+			<?php endforeach; ?>
 
 		<?php else : ?>
 
