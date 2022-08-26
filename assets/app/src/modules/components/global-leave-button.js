c68098e9b7022c15fd2cd3f9b7437328629f26dd
Laurens Offereins <lmoffereins@gmail.com>
2022-08-26 17:44:13 +0200

    Code: unify spaceless inverted boolean values in element attributes

--- assets/app/src/modules/components/global-leave-button.js
+++ assets/app/src/modules/components/global-leave-button.js
@@ -8,7 +8,7 @@ define([
 	"settings"
 ], function( settings ) {
 	return {
-		template: '<button type="button" v-if="! $isLoggedIn" @click="leave" class="icon-button" v-l10n-title.Administration.GoToAdminLabel><i class="dashicons dashicons-exit"></i><span class="screen-reader-text" v-l10n.Administration.GoToAdminLabel></span></button>',
+		template: '<button type="button" v-if="!$isLoggedIn" @click="leave" class="icon-button" v-l10n-title.Administration.GoToAdminLabel><i class="dashicons dashicons-exit"></i><span class="screen-reader-text" v-l10n.Administration.GoToAdminLabel></span></button>',
 		methods: {
 			/**
 			 * Leave handler
