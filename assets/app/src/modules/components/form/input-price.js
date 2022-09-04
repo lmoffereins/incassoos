d874764f380c4b0509f518e24e3bb1edb104a75e
Laurens Offereins <lmoffereins@gmail.com>
2022-09-04 17:14:13 +0200

    App: move price form input to text type with custom validation, because type number does not properly support decimal input with commas and periods.

--- assets/app/src/modules/components/form/input-price.js
+++ assets/app/src/modules/components/form/input-price.js
@@ -1,6 +1,8 @@
 /**
  * Input Price Component
  *
+ * @link https://www.filamentgroup.com/lab/type-number.html
+ *
  * @package Incassoos
  * @subpackage App/Components/Form
  */
@@ -21,10 +23,38 @@ define([
 			/**
 			 * Emit the input's value, but debounced
 			 *
+			 * @param  {Number} addValue Value to add to input
+			 * @return {Void}
+			 */
+			_emit: function( addValue ) {
+				var value = this.$refs.price.value,
+				    str = value.toString(),
+				    addValue = addValue || 0;
+
+				// Bail when last character is a decimal separator
+				if (-1 !== [".", ","].indexOf(str.charAt(str.length - 1))) {
+					return;
+				}
+
+				// Strip non-numeric characters, except decimal pointers and negative sign
+				value = value.replace(/[^\d,.-]/g, "");
+				value = (parseFloat(value.replace(",", ".")) + addValue) || addValue;
+				value = util.sanitizePrice(value);
+
+				// Set input sanitized input value
+				this.$refs.price.value = value;
+
+				// Emit input value
+				this.$emit("input", value);
+			},
+
+			/**
+			 * Emit the input's value, but debounced
+			 *
 			 * @return {Void}
 			 */
 			input: function() {
-				this.$emit("input", util.sanitizePrice(this.$refs.price.value));
+				this._emit();
 			},
 
 			/**
@@ -33,7 +63,7 @@ define([
 			 * @return {Void}
 			 */
 			plus010: function() {
-				this.$emit("input", util.sanitizePrice(parseFloat(this.$refs.price.value) + 0.1));
+				this._emit(0.1);
 			},
 
 			/**
@@ -42,7 +72,7 @@ define([
 			 * @return {Void}
 			 */
 			plus100: function() {
-				this.$emit("input", util.sanitizePrice(parseFloat(this.$refs.price.value) + 1));
+				this._emit(1);
 			},
 
 			/**
@@ -51,7 +81,7 @@ define([
 			 * @return {Void}
 			 */
 			minus010: function() {
-				this.$emit("input", util.sanitizePrice(parseFloat(this.$refs.price.value) - 0.1));
+				this._emit(-0.1);
 			},
 
 			/**
@@ -60,7 +90,7 @@ define([
 			 * @return {Void}
 			 */
 			minus100: function() {
-				this.$emit("input", util.sanitizePrice(parseFloat(this.$refs.price.value) - 1));
+				this._emit(-1);
 			}
 		}
 	};
