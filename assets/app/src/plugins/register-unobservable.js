6b114c46c27a90d7d7c1b9bb7c3eb61c1c302b3f
Laurens Offereins <lmoffereins@gmail.com>
2022-10-12 10:03:30 +0200

    App: documentation updates for the Register-Unobservable Vue plugin

--- assets/app/src/plugins/register-unobservable.js
+++ assets/app/src/plugins/register-unobservable.js
@@ -1,10 +1,9 @@
 /**
- * Vue Register-Unobserver plugin
+ * Vue Register-Unobservable plugin
  *
- * Unobservers are automatically called on component destruction. This
- * leaves unregistering of event handlers or other listeners to the
- * component, while not having to use hacks in order to make the unregistration
- * function available to the component's `beforeDestroy()` method.
+ * Registers a helper method for running unobserve callbacks in a component's
+ * `beforeDestroy()` lifecycle hook. This removes the need for using hacks to
+ * make the unobserve callback manually available on component destruction.
  *
  * @author Laurens Offereins
  * @since 2019-05-16
@@ -14,22 +13,23 @@ define([], function() {
 	/**
 	 * Add unobserver to the list of unobservers
 	 *
-	 * @param {Function} fn Unobserver function
+	 * @param {Function} callback Unobserver callback
 	 * @return {Void}
 	 */
-	var registerUnobservable = function( fn ) {
-		this._unobservers.push(fn);
+	var registerUnobservable = function( callback ) {
+		this._unobservers.push(callback);
 	};
 
 	return {
 		/**
 		 * Plugin installer
 		 *
+		 * This plugin has no options.
+		 *
 		 * @param  {Object} Vue The Vue instance
-		 * @param  {Object} options Optional parameters
 		 * @return {Void}
 		 */
-		install: function( Vue, options ) {
+		install: function( Vue ) {
 			Vue.mixin({
 				/**
 				 * Register unobserver handlers when the component is about to be created
@@ -49,9 +49,9 @@ define([], function() {
 				beforeDestroy: function() {
 					var self = this;
 
-					// Unobserve all registered observers
-					this._unobservers.forEach( function( unobserver ) {
-						("function" === typeof unobserver) && unobserver();
+					// Run the registered unobserver callbacks
+					this._unobservers.forEach( function( callback ) {
+						("function" === typeof callback) && callback();
 					});
 				}
 			});
